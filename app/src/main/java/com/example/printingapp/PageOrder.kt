de952294bd46d32c5f73afa7c54a4a610f73863d
AlanaNabihahT <katyushavanne@gmail.com>
2024-06-09 23:16:03 +0700

    Update salah buat activity tadi

--- app/src/main/java/com/example/printingapp/PageOrder.kt
+++ app/src/main/java/com/example/printingapp/PageOrder.kt
@@ -2,7 +2,13 @@ package com.example.printingapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +24,16 @@ class PageOrder : AppCompatActivity() {
         val button = findViewById<ImageButton>(R.id.backByrBtn).setOnClickListener {
             val intent = Intent(this,  //isi sama page yang buat upload file)
         }
+
+        val button = findViewById<Button>(R.id.byrButton).setOnClickListener {
+            val intent = Intent(this,  //isi sama page yang proses order)
+        }
+
+        val spinnerId =findViewById<Spinner>(R.id.pilihKirim)
+        val jaskir = arrayOf("Hemat", "Reguler", "Kilat")
+        val arrayAdp =
+            ArrayAdapter(this@PageOrder, android.R.layout.simple_spinner_dropdown_item, jaskir)
+        spinnerId.adapter = arrayAdp
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
