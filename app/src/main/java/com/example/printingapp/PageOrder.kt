4a068ef1de803a1e4b8cccbe6e54c63fbac0ec4c
AlanaNabihahT <katyushavanne@gmail.com>
2024-06-09 22:21:36 +0700

    Update salah buat activity tadi

--- app/src/main/java/com/example/printingapp/PageOrder.kt
+++ app/src/main/java/com/example/printingapp/PageOrder.kt
@@ -1,20 +1,30 @@
 package com.example.printingapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PageOrder : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_page_order)
+
+        val button = findViewById<ImageButton>(R.id.backByrBtn).setOnClickListener {
+            val intent = Intent(this,  //isi sama page yang buat upload file)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
